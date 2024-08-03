913a940e9e72d0c8f11f78dc1bedb33f95b940f5
soxo <919006445@qq.com>
2024-08-03 13:00:07 +0800

    Update index.ts

--- src/directives/index.ts
+++ src/directives/index.ts
@@ -1,11 +1,11 @@
-<!--
+/*
  * @Description: Copyright
  * @Author: 简艾
  * @Email: 137333@163.com
  * @Date: 2017-12-15 16:51:10
  * @LastEditTime: 2024-07-30 19:37:31
  * @LastEditors: 简艾
--->
+*/
 
 import ripple from "./ripple";
 
