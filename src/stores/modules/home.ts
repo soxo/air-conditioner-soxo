0d62df67d762aa21496aa1ceb0c7a83f4bd6796e
soxo <919006445@qq.com>
2024-08-03 13:03:35 +0800

    Update home.ts

--- src/stores/modules/home.ts
+++ src/stores/modules/home.ts
@@ -1,11 +1,11 @@
 /*
  * @Description: 首页数据
- * @Author: 安知鱼
- * @Email: anzhiyu-c@qq.com
- * @Date: 2022-10-31 17:42:11
- * @LastEditTime: 2022-11-30 09:05:09
- * @LastEditors: 安知鱼
- */
+ * @Author: 简艾
+ * @Email: 137333@163.com
+ * @Date: 2017-12-15 16:51:10
+ * @LastEditTime: 2024-07-30 19:37:31
+ * @LastEditors: 简艾
+*/
 import { defineStore } from "pinia";
 import Snackbar from "node-snackbar";
 type AcMode = "clod" | "hot";
