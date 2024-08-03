0d46dbd4454e5dfc9b0ed638d24dc81bc5980f8f
soxo <919006445@qq.com>
2024-08-03 12:55:22 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <!--
- * @Description: APP
- * @Author: 安知鱼
- * @Email: anzhiyu-c@qq.com
- * @Date: 2022-10-31 13:27:51
- * @LastEditTime: 2022-11-30 08:51:24
- * @LastEditors: 安知鱼
+ * @Description: Copyright
+ * @Author: 简艾
+ * @Email: 137333@163.com
+ * @Date: 2017-12-15 16:51:10
+ * @LastEditTime: 2024-07-30 19:37:31
+ * @LastEditors: 简艾
 -->
+
 <script setup lang="ts">
 import MainTop from "./components/main-top.vue";
 import MainContent from "./components/main-content.vue";
